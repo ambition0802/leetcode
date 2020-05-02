5e35550ba5d5dda7883bd9478f2907a68973cdf7
sid <673088860@qq.com>
2020-05-02 09:54:13 +0800

    增加leetcode笔记专用图库

--- src/main/java/com/huang/leecode/dynaminc_planning/knapsack/PartitionEqualSubsetSum416.java
+++ src/main/java/com/huang/leecode/dynaminc_planning/knapsack/PartitionEqualSubsetSum416.java
@@ -7,9 +7,15 @@ public class PartitionEqualSubsetSum416 {
 
 
     public static void main(String[] args) {
-        new PartitionEqualSubsetSum416().canPartition(new int[]{1,5,11,5});
+        new PartitionEqualSubsetSum416().canPartition2(new int[]{1,5,11,5});
     }
 
+    /**
+     * 二维DP数组
+     *
+     * @param nums
+     * @return
+     */
     public boolean canPartition(int[] nums) {
 
         //数组中所有成员的总大小
@@ -48,5 +54,43 @@ public class PartitionEqualSubsetSum416 {
         return false;
     }
 
+
+    /**
+     * 一维DP数组
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartition2(int[] nums) {
+
+        //数组中所有成员的总大小
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        //因为数组会被拆分成两个数组，且这两个数组的成员之和相等
+        //所以背包的总容量大小就为数组的“总容量”大小的一半
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int knapsackCap = sum / 2;
+
+
+
+        //f[i][j]这个二维数组表示,只从nums的前i个内取成员，每个成员只能取一次，成员之和的上限为j时的最大成员之和
+        int[] dp = new int[knapsackCap + 1];
+        for (int i= 0; i<nums.length; i++) {
+            for (int j= knapsackCap; j > 0 ; j--) {
+                dp[j] =  dp[j - 1] + nums[i];
+                if (dp[knapsackCap] == knapsackCap) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 
 }
